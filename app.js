72c2e7ade5c0626e37f703843185004782880677
William0832 <squall0832@gmail.com>
2020-04-30 23:43:31 +0800

    Add admin restaurant index page

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const handlebars = require('express-handlebars')
 
 const app = express()
 const port = 3000
-
+// 設定 view engine
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
-
+// 引入 routes 並輸入 app 物件 來指定路由
 require('./routes')(app)
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const restController = require('../controllers/restController.js')
+const adminController = require('../controllers/adminController.js')
+
 module.exports = (app) => {
+  // 前台路由
   app.get('/', (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', restController.getRestaurants)
+  // 後臺路由
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', adminController.getRestaurants)
 }
