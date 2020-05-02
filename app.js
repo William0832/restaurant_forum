a7e04fc8fd21b8b06a9cf3372ebf46cd6310bc16
William0832 <squall0832@gmail.com>
2020-05-03 02:22:08 +0800

    Add userController and signup page

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models') // 引入資料庫
+const bodyParser = require('body-parser') 
 
 const app = express()
 const port = 3000
 // 設定 view engine
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
