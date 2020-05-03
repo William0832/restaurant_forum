62b774a1a41cca4736dac1bf33b688edea854783
William0832 <squall0832@gmail.com>
2020-05-03 18:34:48 +0800

    Install pg & updata production config & listen to process.env.PORT

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const passport = require('./config/passport')
 const methodOverride = require('method-override')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 // 設定 view engine
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
