fa5b48d81dbea03d4dd78b62868437c2b42e0a84
William0832 <squall0832@gmail.com>
2020-05-09 03:28:05 +0800

    add filiter on category naverbar

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,16 +2,26 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 const restController = {
-  getRestaurants: (req, res) =>
-    Restaurant.findAll({ include: Category }).then((restaurants) => {
-      const data = restaurants.map((r) => ({
-        ...r.dataValues,
-        description: r.dataValues.description.substring(0, 50)
-      }))
-      return Category.findAll({ raw: true, nest: true }).then((categories) =>
-        res.render('restaurants', { restaurants: data, categories: categories })
-      )
-    }),
+  getRestaurants: (req, res) => {
+    let whereQuery = {}
+    if (req.query.categoryId) {
+      whereQuery['CategoryId'] = Number(req.query.categoryId)
+    }
+    Restaurant.findAll({ include: Category, where: whereQuery }).then(
+      (restaurants) => {
+        const data = restaurants.map((r) => ({
+          ...r.dataValues,
+          description: r.dataValues.description.substring(0, 50)
+        }))
+        return Category.findAll({ raw: true, nest: true }).then((categories) =>
+          res.render('restaurants', {
+            restaurants: data,
+            categories: categories
+          })
+        )
+      }
+    )
+  },
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category
