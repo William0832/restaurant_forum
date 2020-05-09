a60e4cfb7b9ce4ca62c84ecc8a85d20b5c0beb56
William0832 <squall0832@gmail.com>
2020-05-10 02:35:25 +0800

    Convert time format using moment

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,8 +1,10 @@
+const moment = require('moment')
 module.exports = {
   ifCond: (a, b, options) => {
     if (a === b) {
       return options.fn(this)
     }
     return options.inverse(this)
-  }
+  },
+  moment: (a) => moment(a).fromNow()
 }
