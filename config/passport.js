a2e171fa118c4f844cf0c80b9a1a6ba6fc4c241c
William0832 <squall0832@gmail.com>
2020-05-10 22:06:01 +0800

    Create fellowship model and add followers and followings to users

--- config/passport.js
+++ config/passport.js
@@ -44,7 +44,9 @@ passport.deserializeUser((id, cb) => {
   User.findByPk(id, {
     include: [
       { model: db.Restaurant, as: 'FavoritedRestaurants' },
-      { model: db.Restaurant, as: 'LikedRestaurants' }
+      { model: db.Restaurant, as: 'LikedRestaurants' },
+      { model: db.Users, as: 'Followers' },
+      { model: db.User, as: 'Followings' }
     ]
   }).then((user) => {
     user = user.toJSON()
