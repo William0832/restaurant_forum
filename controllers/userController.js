f31296bf3dd28240424905a3b0a07df935971454
William0832 <squall0832@gmail.com>
2020-05-10 15:24:52 +0800

    add comments in uer profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
-// const fs = require('fs')
+const Comment = db.Comment
+const Restaurant = db.Restaurant
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = '93d57282fdfc93a'
 
@@ -54,12 +55,18 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res) => {
-    return User.findByPk(req.params.id)
+    return User.findByPk(req.params.id, {
+      include: [{model: Comment, include: [Restaurant]}]
+    })
       .then((user) => {
+        const comments = user.toJSON().Comments
+        const numOfComments = comments.length
         let isRightUser = req.user.id == user.id ? true : false
         return res.render('user', {
           user: user.toJSON(),
-          isRightUser: isRightUser
+          isRightUser: isRightUser,
+          comments: comments,
+          numOfComments: numOfComments
         })
       })
   },
